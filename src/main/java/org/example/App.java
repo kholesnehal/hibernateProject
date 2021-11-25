2cccb9d30762c3de13a46f5e0cda9a1a76cabbdb
kholesnehal <snehal.khole@perennialsys.com>
2021-11-25 18:44:52 +0530

    hibernate code

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,31 +1,48 @@
 package org.example;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
+import java.util.Date;
+
 
 public class App 
 {
+    private static final Logger logger = LogManager.getLogger(App.class);
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
         Configuration cfg= new Configuration() ;
 
            cfg.configure("hibernate.cfg.xml");
         SessionFactory factory=cfg.buildSessionFactory();
 
        Student student=new Student();
-       student.setId(101);
-       student.setName("Snehal");
-       student.setCity("Pune");
-        System.out.println(student);
+       student.setId(102);
+       student.setName("Manu");
+       student.setCity("Nagar");
+       logger.info(student);
+
+
+
+        Address address=new Address();
+        address.setAddressId(207);
+        address.setStreet("street1");
+        address.setCity("Pune");
+        address.setAddedDate(new Date());
+        address.setOpen(true);
+        address.setX(113.435);
+
+
         Session session=factory.openSession();
 
         session.beginTransaction();
 
         session.save(student);
+        session.save(address);
 
         session.getTransaction().commit();
 
--- src/main/java/org/example/Student.java
+++ src/main/java/org/example/Student.java
@@ -2,11 +2,11 @@ package org.example;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+
 @Data
 @Entity
+
 public class Student {
     @Id
     private int id;
